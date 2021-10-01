bc08c5e2a48de6aa4afabff1c63a3ef2b381618f
kingnaldoo <reinaldosn2016@gmail.com>
2021-10-01 17:35:20 -0300

    initial config

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Routes />
+</template>
+
+<script lang="ts">
+import Routes from './Routes.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Routes
+  },
+};
+</script>
---
+++ src/Routes.vue
@@ -0,0 +1,15 @@
+<template>
+    <Standart />
+</template>
+
+<script lang="ts">
+import Standart from './pages/standart.vue';
+
+export default {
+    name: 'Routes',
+    components: {
+        Standart
+    }
+};
+
+</script>
